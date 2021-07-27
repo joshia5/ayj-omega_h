6925d64e675a50d509b480d68e9eb66f7e6120f7
joshia5 <joshia5@rpi.edu>
2021-07-27 17:39:10 -0400

    working curve test for edges of triangle

--- src/curve_test.cpp
+++ src/curve_test.cpp
@@ -41,66 +41,158 @@ void test_2d(Library *lib, const std::string &mesh_file, const char* vtu_file) {
   Real centerx = 0.5;
   Real centery = 0.5;
   Real R = 0.70711;
-
-  // edge 1 (diameter, 0 to PI)
   Real xi_1 = 0.2748;
   Real xi_2 = 0.7252;
-  Real s_c1 = PI * xi_1;
-  Real s_c2 = PI * xi_2;
 
-  Real x1 = centerx - R*cos(s_c1);
-  Real y1 = centery + R*sin(s_c1);
+  std::ofstream edge_file;
+  edge_file.open("edges.csv");
+  // edge 1 (diameter, starts at pi/4, on for pi)
 
-  Real x2 = centerx - R*cos(s_c2);
-  Real y2 = centery + R*sin(s_c2);
+  {
+    Real s_c1 = PI * xi_1;
+    Real s_c2 = PI * xi_2;
+    Matrix<2,1> p0({0, 1});//ask coords
+    Matrix<2,1> p1({1, 0});
+    Real x1 = centerx - R*cos(PI/4 + s_c1);
+    Real y1 = centery + R*sin(PI/4 + s_c1);
+    Real x2 = centerx - R*cos(PI/4 + s_c2);
+    Real y2 = centery + R*sin(PI/4 + s_c2);
 
-  Matrix<2,1> fx({x1, x2});
-  Matrix<2,1> fy({y1, y2});
+    Matrix<2,1> fx({x1, x2});
+    Matrix<2,1> fy({y1, y2});
 
-  Matrix<2,2> M1_inv ({B1(xi_1), B2(xi_1), B1(xi_2), B2(xi_2)});
-  auto M1 = invert(M1_inv);
-  Matrix<2,2> M2 ({B0(xi_1), B3(xi_1), B0(xi_2), B3(xi_2)});
+    Matrix<2,2> M1_inv ({B1(xi_1), B2(xi_1), B1(xi_2), B2(xi_2)});
+    auto M1 = invert(M1_inv);
+    Matrix<2,2> M2 ({B0(xi_1), B3(xi_1), B0(xi_2), B3(xi_2)});
 
-  Matrix<2,1> p0({0, 1});//ask coords
-  Matrix<2,1> p1({1, 0});
-  auto Cx = M1*fx - M1*M2*p0;
-  auto Cy = M1*fy - M1*M2*p1;
+    auto Cx = M1*fx - M1*M2*p0;
+    auto Cy = M1*fy - M1*M2*p1;
 
-  auto u = Read<Real>(1000, 0.0, 0.001, "samplePts");
-  auto u_h = HostRead<Real>(u);
-  auto u_ex = Read<Real>(1000, 0.0 + PI/4, PI/1000, "exactPts");
-  auto u_ex_h = HostRead<Real>(u_ex);
-  std::ofstream edge1_file;
-  edge1_file.open("edge1.csv");
+    auto u = Read<Real>(1000, 0.0, 0.001, "samplePts");
+    auto u_h = HostRead<Real>(u);
+    auto u_ex = Read<Real>(1000, PI/4, 2*PI/1000, "exactPts");
+    auto u_ex_h = HostRead<Real>(u_ex);
 
-  edge1_file << "x, y\n";
-  for (LO i = 0; i < u.size(); ++i) {
-    auto x_bezier = 0*B0(u[i]) + Cx(0,0)*B1(u[i]) + Cx(1,0)*B2(u[i]) + 1*B3(u[i]);
-    auto y_bezier = 1*B0(u[i]) + Cy(0,0)*B1(u[i]) + Cy(1,0)*B2(u[i]) + 0*B3(u[i]);
-    edge1_file << x_bezier << ", " << y_bezier << "\n";
+    edge_file << "x, y\n";
+    for (LO i = 0; i < u.size(); ++i) {
+      auto x_bezier = p0(0,0)*B0(u[i]) + Cx(0,0)*B1(u[i]) + Cx(1,0)*B2(u[i]) + p1(0,0)*B3(u[i]);
+      auto y_bezier = p0(1,0)*B0(u[i]) + Cy(0,0)*B1(u[i]) + Cy(1,0)*B2(u[i]) + p1(1,0)*B3(u[i]);
+      edge_file << x_bezier << ", " << y_bezier << "\n";
+    }
+    edge_file << "\n";
+    edge_file << "\n";
+    edge_file << "C1x, C1y\n";
+    edge_file << Cx(0,0) << ", " << Cy(0,0) << "\n";
+    edge_file << "C2x, C2y\n";
+    edge_file << Cx(1,0) << ", " << Cy(1,0) << "\n";
+    edge_file << "x1, y1\n";
+    edge_file << x1 << ", " << y1 << "\n";
+    edge_file << "x2, y2\n";
+    edge_file << x2 << ", " << y2 << "\n";
+    edge_file << "\n";
+    edge_file << "\n";
+    edge_file << "exact circle:\n";
+    edge_file << "x, y\n";
+    for (LO i = 0; i < u_ex.size(); ++i) {
+      auto x_circle = centerx - R*cos(u_ex[i]);
+      auto y_circle = centery + R*sin(u_ex[i]);
+      edge_file << x_circle << ", " << y_circle << "\n";
+    }
   }
-  edge1_file << "\n";
-  edge1_file << "\n";
-  edge1_file << "C1x, C1y\n";
-  edge1_file << Cx(0,0) << ", " << Cy(0,0) << "\n";
-  edge1_file << "C2x, C2y\n";
-  edge1_file << Cx(1,0) << ", " << Cy(1,0) << "\n";
-  edge1_file << "x1, y1\n";
-  edge1_file << x1 << ", " << y1 << "\n";
-  edge1_file << "x2, y2\n";
-  edge1_file << x2 << ", " << y2 << "\n";
-  edge1_file << "\n";
-  edge1_file << "\n";
-  edge1_file << "exact circle:\n";
-  edge1_file << "x, y\n";
-  for (LO i = 0; i < u_ex.size(); ++i) {
-    auto x_circle = centerx - R*cos(u_ex[i]);
-    auto y_circle = centery + R*sin(u_ex[i]);
-    edge1_file << x_circle << ", " << y_circle << "\n";
-  }
+  edge_file << "\n";
 
-  edge1_file.close();
   //
+
+  // edge 2 (along x, starts at 5*pi/4 goes on for pi/2)
+  {
+    Real s_c1 = PI/2 * xi_1;
+    Real s_c2 = PI/2 * xi_2;
+    Matrix<2,1> p0({1, 0});//ask coords
+    Matrix<2,1> p1({0, 0});
+    Real x1 = centerx - R*cos(5*PI/4 + s_c1);
+    Real y1 = centery + R*sin(5*PI/4 + s_c1);
+    Real x2 = centerx - R*cos(5*PI/4 + s_c2);
+    Real y2 = centery + R*sin(5*PI/4 + s_c2);
+
+    Matrix<2,1> fx({x1, x2});
+    Matrix<2,1> fy({y1, y2});
+
+    Matrix<2,2> M1_inv ({B1(xi_1), B2(xi_1), B1(xi_2), B2(xi_2)});
+    auto M1 = invert(M1_inv);
+    Matrix<2,2> M2 ({B0(xi_1), B3(xi_1), B0(xi_2), B3(xi_2)});
+
+    auto Cx = M1*fx - M1*M2*p0;
+    auto Cy = M1*fy - M1*M2*p1;
+
+    auto u = Read<Real>(1000, 0.0, 0.001, "samplePts");
+    auto u_h = HostRead<Real>(u);
+
+    edge_file << "for edge 2\n";
+    edge_file << "x, y\n";
+    for (LO i = 0; i < u.size(); ++i) {
+      auto x_bezier = p0(0,0)*B0(u[i]) + Cx(0,0)*B1(u[i]) + Cx(1,0)*B2(u[i]) + p1(0,0)*B3(u[i]);
+      auto y_bezier = p0(1,0)*B0(u[i]) + Cy(0,0)*B1(u[i]) + Cy(1,0)*B2(u[i]) + p1(1,0)*B3(u[i]);
+      edge_file << x_bezier << ", " << y_bezier << "\n";
+    }
+    edge_file << "\n";
+    edge_file << "\n";
+    edge_file << "C1x, C1y\n";
+    edge_file << Cx(0,0) << ", " << Cy(0,0) << "\n";
+    edge_file << "C2x, C2y\n";
+    edge_file << Cx(1,0) << ", " << Cy(1,0) << "\n";
+    edge_file << "x1, y1\n";
+    edge_file << x1 << ", " << y1 << "\n";
+    edge_file << "x2, y2\n";
+    edge_file << x2 << ", " << y2 << "\n";
+  }
+  edge_file << "\n";
+
+  // edge 3 (along y, starts at 7*pi/4 goes on for pi/2)
+  {
+    Real s_c1 = PI/2 * xi_1;
+    Real s_c2 = PI/2 * xi_2;
+    Matrix<2,1> p0({0, 0});//ask coords
+    Matrix<2,1> p1({0, 1});
+    Real x1 = centerx - R*cos(7*PI/4 + s_c1);
+    Real y1 = centery + R*sin(7*PI/4 + s_c1);
+    Real x2 = centerx - R*cos(7*PI/4 + s_c2);
+    Real y2 = centery + R*sin(7*PI/4 + s_c2);
+
+    Matrix<2,1> fx({x1, x2});
+    Matrix<2,1> fy({y1, y2});
+
+    Matrix<2,2> M1_inv ({B1(xi_1), B2(xi_1), B1(xi_2), B2(xi_2)});
+    auto M1 = invert(M1_inv);
+    Matrix<2,2> M2 ({B0(xi_1), B3(xi_1), B0(xi_2), B3(xi_2)});
+
+    auto Cx = M1*fx - M1*M2*p0;
+    auto Cy = M1*fy - M1*M2*p1;
+
+    auto u = Read<Real>(1000, 0.0, 0.001, "samplePts");
+    auto u_h = HostRead<Real>(u);
+
+    edge_file << "for edge 3\n";
+    edge_file << "x, y\n";
+    for (LO i = 0; i < u.size(); ++i) {
+      auto x_bezier = p0(0,0)*B0(u[i]) + Cx(0,0)*B1(u[i]) + Cx(1,0)*B2(u[i]) + p1(0,0)*B3(u[i]);
+      auto y_bezier = p0(1,0)*B0(u[i]) + Cy(0,0)*B1(u[i]) + Cy(1,0)*B2(u[i]) + p1(1,0)*B3(u[i]);
+      edge_file << x_bezier << ", " << y_bezier << "\n";
+    }
+    edge_file << "\n";
+    edge_file << "\n";
+    edge_file << "C1x, C1y\n";
+    edge_file << Cx(0,0) << ", " << Cy(0,0) << "\n";
+    edge_file << "C2x, C2y\n";
+    edge_file << Cx(1,0) << ", " << Cy(1,0) << "\n";
+    edge_file << "x1, y1\n";
+    edge_file << x1 << ", " << y1 << "\n";
+    edge_file << "x2, y2\n";
+    edge_file << x2 << ", " << y2 << "\n";
+  }
+  edge_file << "\n";
+
+  edge_file.close();
+
   return;
 }
 
