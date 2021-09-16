a1934a383873b11686f84afe1aa05c933f09074a
joshia5 <joshia5@rpi.edu>
2021-09-16 15:17:21 -0400

    calc ctrl pts for edge 0 (as per template)
    wip

--- src/Omega_h_beziers.cpp
+++ src/Omega_h_beziers.cpp
@@ -2,6 +2,7 @@
 #include "Omega_h_beziers.hpp"
 #include "Omega_h_element.hpp"
 #include "Omega_h_for.hpp"
+#include "Omega_h_atomics.hpp"
 
 namespace Omega_h {
 
@@ -298,7 +299,7 @@ void elevate_curve_order_3to4(Mesh* mesh) {
       new_pts[i*n_new_pts*dim + 2*dim + d] = c3[d];
     }
   };
-  parallel_for(nedge, calc_pts);
+  parallel_for(nedge, std::move(calc_pts));
 
   mesh->set_tag_for_ctrlPts(1, Reals(new_pts));
 
@@ -421,7 +422,7 @@ void elevate_curve_order_3to4(Mesh* mesh) {
       face_pts[i*n_new_face_pts*dim + dim + dim + d] = c12[d];
     }
   };
-  parallel_for(nface, calc_face_pts);
+  parallel_for(nface, std::move(calc_face_pts));
   mesh->set_tag_for_ctrlPts(2, Reals(face_pts));
 
   //formulae to calc pts inside regions?
@@ -463,7 +464,7 @@ void elevate_curve_order_4to5(Mesh* mesh) {
       new_pts[i*n_new_pts*dim + 3*dim + d] = c4[d];
     }
   };
-  parallel_for(nedge, calc_pts);
+  parallel_for(nedge, std::move(calc_pts));
 
   mesh->set_tag_for_ctrlPts(1, Reals(new_pts));
   return;
@@ -507,9 +508,124 @@ void elevate_curve_order_5to6(Mesh* mesh) {
       new_pts[i*n_new_pts*dim + 4*dim + d] = c5[d];
     }
   };
-  parallel_for(nedge, calc_pts);
+  parallel_for(nedge, std::move(calc_pts));
 
   mesh->set_tag_for_ctrlPts(1, Reals(new_pts));
+  return;
+}
+
+void transfer_bezier_edges(Mesh *mesh, Mesh *new_mesh,
+    LOs old2new, LOs prods2new, LOs keys2prods,
+    LOs keys2midverts, LOs keys2edges) {
+  
+  LO count_key = 0;
+  auto nold_edge = old2new.size();
+  auto old_ev2v = mesh->get_adj(1, 0).ab2b;
+  auto new_ev2v = new_mesh->get_adj(1, 0).ab2b;
+  auto old_coords = mesh->coords();
+  auto new_coords = new_mesh->coords();
+  auto n_edge_pts = mesh->n_internal_ctrlPts(1);
+  auto order = mesh->get_max_order;
+  auto dim = mesh->dim();
+  auto old_ctrlPts = mesh->get_ctrlPts(1);
+  auto nnew_edge = new_ev2v.size()/2;
+  Write<Real> edge_ctrlPts(nnew_edge*n_edge_pts*dim);
+
+  auto transfer_edges = OMEGA_H_LAMBDA (LO old_edge) {
+    v0_old = old_ev2v[old_edge*2 + 0];
+    v1_old = old_ev2v[old_edge*2 + 1];
+
+    if (old2new[i] != -1) {
+      new_edge = old2new[i];
+      // map as is from old id to new id
+    }
+    else {
+      key_id = count_key;
+      mid_vert = keys2midverts[key_id];
+      start = prods2new[i];
+      end = prods2new[i+1] - 1;
+      OMEGA_H_CHECK((end-start) == 2);
+      new_e0 = prods2new[start];
+      new_e1 = prods2new[start+1];
+      new_e2 = prods2new[start+2];
+
+      v0_new_e0 = new_ev2v[new_e0*2 + 0];
+      v1_new_e0 = new_ev2v[new_e0*2 + 1];
+      v0_new_e1 = new_ev2v[new_e1*2 + 0];
+      v1_new_e1 = new_ev2v[new_e1*2 + 1];
+      OMEGA_H_CHECK((v1_new_e0 == mid_vert) && (v0_new_e1 == mid_vert));
+      // swap unneeded, order checked
+
+      /*
+      // check flip
+      auto flip_new_e0 = -1;
+      auto flip_new_e1 = -1;
+      */
+
+      //ctrl pts for e0
+      {
+        //for 2d mesh for now, order=3
+        //query old ctrl pts
+        Real cx0 = new_coords[v0_old*dim + 0];
+        Real cy0 = new_coords[v0_old*dim + 1];
+        Real cx1 = old_ctrlPts[i*n_edge_pts*dim + 0];
+        Real cy1 = old_ctrlPts[i*n_edge_pts*dim + 1];
+        Real cx2 = old_ctrlPts[i*n_edge_pts*dim + (n_edge_pts-1)*dim + 0];
+        Real cy2 = old_ctrlPts[i*n_edge_pts*dim + (n_edge_pts-1)*dim + 1];
+        Real cx3 = new_coords[v1_old*dim + 0];
+        Real cy3 = new_coords[v1_old*dim + 1];
+
+        Real new_xi_3 = 0.5;
+        Real new_cx3 = cx0*B0_cube(new_xi_3) + cx1*B1_cube(new_xi_3) +
+                       cx2*B2_cube(new_xi_3) + cx3*B3_cube(new_xi_3);
+        Real new_cy3 = cy0*B0_cube(new_xi_3) + cy1*B1_cube(new_xi_3) +
+                       cy2*B2_cube(new_xi_3) + cy3*B3_cube(new_xi_3);
+
+        Real new_xi_2 = xi_2_cube()/2.0;
+        Real new_px2 = cx0*B0_cube(new_xi_2) + cx1*B1_cube(new_xi_2) +
+                       cx2*B2_cube(new_xi_2) + cx3*B3_cube(new_xi_2);
+        Real new_py2 = cy0*B0_cube(new_xi_2) + cy1*B1_cube(new_xi_2) +
+                       cy2*B2_cube(new_xi_2) + cy3*B3_cube(new_xi_2);
+
+        Real new_xi_1 = xi_1_cube()/2.0;
+        Real new_px1 = cx0*B0_cube(new_xi_1) + cx1*B1_cube(new_xi_1) +
+                       cx2*B2_cube(new_xi_1) + cx3*B3_cube(new_xi_1);
+        Real new_py1 = cy0*B0_cube(new_xi_1) + cy1*B1_cube(new_xi_1) +
+                       cy2*B2_cube(new_xi_1) + cy3*B3_cube(new_xi_1);
+
+        Matrix<2,1> c0({cx0, cy0});
+        Matrix<2,1> c3({new_cx3, new_cy3});
+
+        Matrix<2,1> fx({new_px1, new_px2});
+        Matrix<2,1> fy({new_py1, new_py2});
+
+        Matrix<2,2> M1_inv({B1_cube(new_xi_1), B2_cube(new_xi_1), B1_cube(new_xi_2),
+                            B2_cube(new_xi_2)});
+        auto M1 = invert(M1_inv);
+        Matrix<2,2> M2({B0_cube(new_xi_1), B3_cube(new_xi_1), B0_cube(new_xi_2),
+                        B3_cube(new_xi_2)});
+
+        auto Cx = M1*fx - M1*M2*p0;
+        auto Cy = M1*fy - M1*M2*p1;
+        edge_ctrlPts[new_e0*n_edge_pts*dim + 0] = Cx(0,0);
+        edge_ctrlPts[new_e0*n_edge_pts*dim + 1] = Cy(0,0);
+        edge_ctrlPts[new_e0*n_edge_pts*dim + (n_edge_pts-1)*dim + 0] = Cx(1,0);
+        edge_ctrlPts[new_e0*n_edge_pts*dim + (n_edge_pts-1)*dim + 1] = Cy(1,0);
+
+      }
+      //ctrl pts for e1
+      {
+      }
+      //ctrl pts for e2
+      {
+        inquire old face
+      }
+
+      atomic_fetch_add(&count_key, 1);
+    }
+  };
+  parallel_for(nold_edge, std::move(transfer_edges));
+
   return;
 }
 
