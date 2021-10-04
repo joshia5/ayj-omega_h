afbc60481b554a8112687e56cebefdcb92b6964f
joshia5 <joshia5@rpi.edu>
2021-10-03 22:24:15 -0400

    initial commit for 3d

--- src/Omega_h_beziers.hpp
+++ src/Omega_h_beziers.hpp
@@ -5,13 +5,6 @@
 
 namespace Omega_h {
 
-void elevate_curve_order_2to3(Mesh* mesh);
-void elevate_curve_order_3to4(Mesh* mesh);
-void elevate_curve_order_4to5(Mesh* mesh);
-void elevate_curve_order_5to6(Mesh* mesh);
-
-void calc_quad_ctrlPts_from_interpPts(Mesh *mesh);
-
 Real B0_quad(Real u);
 Real B1_quad(Real u);
 Real B2_quad(Real u);
@@ -75,11 +68,131 @@ Real xi_2_quint();
 Real xi_3_quint();
 Real xi_4_quint();
 
+Reals xi_11_cube();
+
+void elevate_curve_order_2to3(Mesh* mesh);
+void elevate_curve_order_3to4(Mesh* mesh);
+void elevate_curve_order_4to5(Mesh* mesh);
+void elevate_curve_order_5to6(Mesh* mesh);
+
+void calc_quad_ctrlPts_from_interpPts(Mesh *mesh);
+
+OMEGA_H_DEVICE Reals cubic_noKeyEdge_xi_values(LO old_vert, LO v0, LO v1, LO v2,
+                                        LO old_edge, LO e0, LO e1, LO e2) {
+
+  Write<Real> p1_p2(4);
+  if (old_vert == v0) {
+    OMEGA_H_CHECK(old_edge == e1);
+    p1_p2[0] = 0.13740215;
+    p1_p2[1] = 0.13740215;
+    p1_p2[2] = 0.362597849;
+    p1_p2[3] = 0.362597849;
+  }
+  else if (old_vert == v1) {
+    OMEGA_H_CHECK(old_edge == e2);
+    p1_p2[0] = 0.7251957;
+    p1_p2[1] = 0.13740215;
+    p1_p2[2] = 0.2748043;
+    p1_p2[3] = 0.362597849;
+  }
+  else if (old_vert == v2) {
+    OMEGA_H_CHECK(old_edge == e0);
+    p1_p2[0] = 0.13740215;
+    p1_p2[1] = 0.7251957;
+    p1_p2[2] = 0.362597849;
+    p1_p2[3] = 0.2748043;
+  }
+  else {
+    Omega_h_fail("incorrect old face\n");
+  }
+
+ return Reals(p1_p2);
+}
+
+OMEGA_H_DEVICE Reals cubic_face_xi_values
+  (LO const old_noKey_vert, LO const old_v0, LO const old_v1, LO const old_v2,
+   LO const old_key_edge, LO const old_e0, LO const old_e1, LO const old_e2,
+   LO const new_v0_f0, LO const new_v1_f0, LO const new_v2_f0,
+   LO const new_v0_f1, LO const new_v1_f1, LO const new_v2_f1,
+   LO const oldf_v0_new, LO const oldf_v1_new, LO const oldf_v2_new) {
+
+  Write<Real> p1_p2(4);
+  I8 should_swap = -1;
+  if (old_noKey_vert == old_v0) {
+    OMEGA_H_CHECK(old_key_edge == old_e1);
+    p1_p2[0] = 1.0/2.0;
+    p1_p2[1] = 1.0/6.0;
+    p1_p2[2] = 1.0/6.0;
+    p1_p2[3] = 1.0/2.0;
+
+    if ((oldf_v1_new == new_v0_f0) || (oldf_v1_new == new_v1_f0) || 
+        (oldf_v1_new == new_v2_f0)) {
+      should_swap = -1;
+    }
+    else {
+      OMEGA_H_CHECK ((oldf_v1_new == new_v0_f1) || (oldf_v1_new == new_v1_f1) ||
+                     (oldf_v1_new == new_v2_f1));
+      should_swap = 1;
+    }
+  }
+  else if (old_noKey_vert == old_v1) {
+    OMEGA_H_CHECK(old_key_edge == old_e2);
+    p1_p2[0] = 1.0/3.0;
+    p1_p2[1] = 1.0/2.0;
+    p1_p2[2] = 1.0/3.0;
+    p1_p2[3] = 1.0/6.0;
+
+    if ((oldf_v2_new == new_v0_f0) || (oldf_v2_new == new_v1_f0) ||
+        (oldf_v2_new == new_v2_f0)) {
+      should_swap = -1;
+    }
+    else {
+      OMEGA_H_CHECK ((oldf_v2_new == new_v0_f1) || (oldf_v2_new == new_v1_f1) ||
+                     (oldf_v2_new == new_v2_f1));
+      should_swap = 1;
+    }
+  }
+  else if (old_noKey_vert == old_v2) {
+    OMEGA_H_CHECK(old_key_edge == old_e0);
+    p1_p2[0] = 1.0/6.0;
+    p1_p2[1] = 1.0/3.0;
+    p1_p2[2] = 1.0/2.0;
+    p1_p2[3] = 1.0/3.0;
+
+    if ((oldf_v0_new == new_v0_f0) || (oldf_v0_new == new_v1_f0) ||
+        (oldf_v0_new == new_v2_f0)) {
+      should_swap = -1;
+    }
+    else {
+      OMEGA_H_CHECK ((oldf_v0_new == new_v0_f1) || (oldf_v0_new == new_v1_f1) ||
+                     (oldf_v0_new == new_v2_f1));
+      should_swap = 1;
+    }
+  }
+  else {
+    Omega_h_fail("incorrect old face\n");
+  }
+  if (should_swap == 1) {
+    swap2(p1_p2[0], p1_p2[2]);
+    swap2(p1_p2[1], p1_p2[3]);
+  }
+
+  return Reals(p1_p2);
+}
+
 LOs create_curved_verts_and_edges_2d(Mesh *mesh, Mesh *new_mesh, LOs old2new,
                                      LOs prods2new, LOs keys2prods,
                                      LOs keys2midverts, LOs old_verts2new_verts);
 
 void create_curved_faces_2d(Mesh *mesh, Mesh *new_mesh, LOs old2new, LOs prods2new,
+                            LOs keys2prods, LOs keys2edges, LOs keys2old_faces,
+                            LOs old_verts2new_verts);
+
+LOs create_curved_verts_and_edges_3d(Mesh *mesh, Mesh *new_mesh, LOs old2new,
+                                     LOs prods2new, LOs keys2prods,
+                                     LOs keys2midverts, LOs old_verts2new_verts);
+
+void create_curved_faces_3d(Mesh *mesh, Mesh *new_mesh, LOs old2new, LOs prods2new,
                             LOs keys2prods, LOs keys2edges, LOs keys2old_faces,
                             LOs old_verts2new_verts);
 
