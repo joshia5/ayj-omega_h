12fd0d218c10c768f7586d234e84ff6a721114eb
joshia5 <joshia5@rpi.edu>
2021-10-20 00:11:56 -0400

    fns for splitting e0 and e1 in 3d

--- src/Omega_h_bezier_interp.hpp
+++ src/Omega_h_bezier_interp.hpp
@@ -18,7 +18,7 @@ OMEGA_H_INLINE Vector<3> curve_paramToParent_3d(
 
 OMEGA_H_INLINE Few<Real, 6> curve_interpToCtrl_pts_3d(
     Int const order, Vector<3> c0, Vector<3> c3, Vector<3> p1, Vector<3> p2) {
-  Few<Real, 6> p1_p2;
+  Few<Real, 6> c1_c2;
 
   Real const xi_1 = xi_1_cube();
   Real const xi_2 = xi_2_cube();
@@ -37,25 +37,85 @@ OMEGA_H_INLINE Few<Real, 6> curve_interpToCtrl_pts_3d(
   auto Cy = M1*fy - M1*M2*cy;
   auto Cz = M1*fz - M1*M2*cz;
 
-  p1_p2[0] = Cx[0];
-  p1_p2[1] = Cy[0];
-  p1_p2[2] = Cz[0];
-  p1_p2[3] = Cx[1];
-  p1_p2[4] = Cy[1];
-  p1_p2[5] = Cz[1];
-  return p1_p2;
+  c1_c2[0] = Cx[0];
+  c1_c2[1] = Cy[0];
+  c1_c2[2] = Cz[0];
+  c1_c2[3] = Cx[1];
+  c1_c2[4] = Cy[1];
+  c1_c2[5] = Cz[1];
+  return c1_c2;
 }
-// workaround CUDA compiler bug
-#ifdef OMEGA_H_USE_CUDA
-__host__
-#endif
 
-#define OMEGA_H_EXPL_INST_DECL(T)
-OMEGA_H_EXPL_INST_DECL(I8)
-OMEGA_H_EXPL_INST_DECL(I32)
-OMEGA_H_EXPL_INST_DECL(I64)
-OMEGA_H_EXPL_INST_DECL(Real)
-#undef OMEGA_H_EXPL_INST_DECL
+OMEGA_H_DEVICE Few<Real, 9> curve_split_e0_3d(
+    Real const new_xi_start, Reals old_vertCtrlPts, Reals old_edgeCtrlPts,
+    Int const order, LO const v0_old, LO const v1_old, LO const old_edge) {
+
+  LO const dim = 3;
+  OMEGA_H_CHECK(order == 3);
+  auto const n_edge_pts = n_internal_ctrlPts(1, order);
+  auto c0 = get_vector<3>(old_vertCtrlPts, v0_old);
+  auto c1 = vector_3(old_edgeCtrlPts[old_edge*n_edge_pts*dim + 0],
+                     old_edgeCtrlPts[old_edge*n_edge_pts*dim + 1],
+                     old_edgeCtrlPts[old_edge*n_edge_pts*dim + 2]);
+  auto c2 = vector_3(
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 0],
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 1],
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 2]);
+  auto c3 = get_vector<3>(old_vertCtrlPts, v1_old);
+
+  Real const new_xi_3 = new_xi_start + 0.5;
+  auto new_c3 = curve_paramToParent_3d(new_xi_3, c0, c1, c2, c3, order);
+
+  Real const new_xi_2 = new_xi_start + xi_2_cube()/2.0;
+  auto new_p2 = curve_paramToParent_3d(new_xi_2, c0, c1, c2, c3, order);
+  Real const new_xi_1 = new_xi_start + xi_1_cube()/2.0;
+  auto new_p1 = curve_paramToParent_3d(new_xi_1, c0, c1, c2, c3, order);
+
+  /*
+  printf("oldedge %d e0 with v0 %d v1 %d; oldc0 %f %f %f; c1 %f %f %f c2 %f %f %f c3 %f %f %f \n",
+      old_edge, v0_old, v1_old, cx0,cy0,cz0, cx1,cy1,cz1, cx2,cy2,cz2, cx3,cy3,cz3);
+  printf(" new mid interp pt oldedge %d e0 with v0 %d v1 %d is %f %f %f\n",
+      old_edge, v0_old, v1_old, new_cx3, new_cy3, new_cz3);
+  printf("oldedge %d edge 0 p2 %f, %f %f , xi2 %f\n", old_edge, new_px2, new_py2, new_pz2, new_xi_2);
+  printf("oldedge %d edge 0 p1 %f, %f %f , xi1 %f\n", old_edge, new_px1, new_py1, new_pz1, new_xi_1);
+  */
+  auto c1_c2 = curve_interpToCtrl_pts_3d(order, c0, new_c3, new_p1, new_p2);
+  Few<Real, 9> c1_c2_c3;
+  for (LO k = 0; k < dim*2; ++k) {
+    c1_c2_c3[k] = c1_c2[k];
+  }
+  for (LO k = 0; k < dim; ++k) {
+    c1_c2_c3[dim*2 + k] = new_c3[k];
+  }
+  return c1_c2_c3;
+}
+
+OMEGA_H_DEVICE Few<Real, 6> curve_split_e1_3d(
+    Real const new_xi_start, Reals old_vertCtrlPts, Reals old_edgeCtrlPts,
+    Int const order, LO const v0_old, LO const v1_old, LO const old_edge,
+    Vector<3> new_c0) {
+
+  LO const dim = 3;
+  OMEGA_H_CHECK(order == 3);
+  auto const n_edge_pts = n_internal_ctrlPts(1, order);
+  auto c0 = get_vector<3>(old_vertCtrlPts, v0_old);
+  auto c1 = vector_3(old_edgeCtrlPts[old_edge*n_edge_pts*dim + 0],
+                     old_edgeCtrlPts[old_edge*n_edge_pts*dim + 1],
+                     old_edgeCtrlPts[old_edge*n_edge_pts*dim + 2]);
+  auto c2 = vector_3(
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 0],
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 1],
+      old_edgeCtrlPts[old_edge*n_edge_pts*dim + (n_edge_pts-1)*dim + 2]);
+  auto c3 = get_vector<3>(old_vertCtrlPts, v1_old);
+
+  Real const new_xi_2 = new_xi_start + xi_2_cube()/2.0;
+  auto new_p2 = curve_paramToParent_3d(new_xi_2, c0, c1, c2, c3, order);
+  Real const new_xi_1 = new_xi_start + xi_1_cube()/2.0;
+  auto new_p1 = curve_paramToParent_3d(new_xi_1, c0, c1, c2, c3, order);
+
+  auto c1_c2 = curve_interpToCtrl_pts_3d(order, new_c0, c3, new_p1, new_p2);
+  return c1_c2;
+}
 
 } // namespace Omega_h
 
