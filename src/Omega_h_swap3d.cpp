aaf359d2a8fc202e5cdc2a23195f2405598f80ff
joshia5 <joshia5@rpi.edu>
2021-11-04 18:19:48 -0400

    minor bug fix with edge swap due to crv

--- src/Omega_h_swap3d.cpp
+++ src/Omega_h_swap3d.cpp
@@ -73,11 +73,6 @@ static void swap3d_element_based(Mesh* mesh, AdaptOpts const& opts) {
     old_lows2new_lows = old_ents2new_ents;
   }
 
-  new_mesh.add_tags_for_ctrlPts();
-  new_mesh.set_tag_for_ctrlPts(0, mesh->get_ctrlPts(0));
-  new_mesh.set_tag_for_ctrlPts(1, mesh->get_ctrlPts(1));
-  new_mesh.set_tag_for_ctrlPts(2, mesh->get_ctrlPts(2));
-
   *mesh = new_mesh;
 }
 
