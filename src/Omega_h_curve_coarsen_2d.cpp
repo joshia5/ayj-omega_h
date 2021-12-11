e19c4051fb83ba1b8ee12d3839dbf25776e87a26
joshia5 <joshia5@rpi.edu>
2021-12-11 15:41:26 -0500

    initial commit for coarsen; copy ctrl pts for vertex

---
+++ src/Omega_h_curve_coarsen_2d.cpp
@@ -0,0 +1,74 @@
+#include "Omega_h_mesh.hpp"
+#include "Omega_h_beziers.hpp"
+#include "Omega_h_element.hpp"
+#include "Omega_h_for.hpp"
+#include "Omega_h_atomics.hpp"
+#include "Omega_h_map.hpp"
+#include "Omega_h_array_ops.hpp"
+#include "Omega_h_vector.hpp"
+#include "Omega_h_matrix.hpp"
+
+namespace Omega_h {
+
+LOs coarsen_curved_verts_and_edges_2d(Mesh *mesh, Mesh *new_mesh, LOs old2new,
+                                     LOs prods2new, LOs keys2prods,
+                                     LOs keys2midverts, LOs old_verts2new_verts,
+                                     LOs keys2edges) {
+  printf("in coarsen curved edges fn\n");
+  OMEGA_H_TIME_FUNCTION;
+  auto const nold_edge = old2new.size();
+  auto const nold_verts = mesh->nverts();
+  OMEGA_H_CHECK(nold_verts == old_verts2new_verts.size());
+  auto const old_ev2v = mesh->get_adj(1, 0).ab2b;
+  auto const old_fe2e = mesh->get_adj(2, 1).ab2b;
+  auto const old_ef2f = mesh->ask_up(1, 2).ab2b;
+  auto const old_e2ef = mesh->ask_up(1, 2).a2ab;
+  auto const old_fv2v = mesh->ask_down(2, 0).ab2b;
+  auto const old_vertCtrlPts = mesh->get_ctrlPts(0);
+  auto const old_edgeCtrlPts = mesh->get_ctrlPts(1);
+  auto const old_faceCtrlPts = mesh->get_ctrlPts(2);
+  auto const order = mesh->get_max_order();
+  auto const dim = mesh->dim();
+  auto const n_edge_pts = mesh->n_internal_ctrlPts(1);
+
+  auto const new_ev2v = new_mesh->get_adj(1, 0).ab2b;
+  auto const new_coords = new_mesh->coords();
+  auto const nnew_edge = new_mesh->nedges();
+  auto const nnew_verts = new_mesh->nverts();
+
+  Write<Real> edge_ctrlPts(nnew_edge*n_edge_pts*dim, INT8_MAX);
+  Write<Real> vert_ctrlPts(nnew_verts*1*dim, INT8_MAX);
+
+  auto new_verts2old_verts = invert_map_by_atomics(old_verts2new_verts,
+                                                   nnew_verts);
+
+  //copy ctrl pts for same verts
+  auto copy_sameCtrlPts = OMEGA_H_LAMBDA(LO i) {
+    if (old_verts2new_verts[i] != -1) {
+      LO new_vert = old_verts2new_verts[i];
+      for (I8 d = 0; d < dim; ++d) {
+        vert_ctrlPts[new_vert*dim + d] = old_vertCtrlPts[i*dim + d];
+      }
+    }
+  };
+  parallel_for(nold_verts, std::move(copy_sameCtrlPts), "copy same vtx ctrlPts");
+  new_mesh->set_tag_for_ctrlPts(0, Reals(vert_ctrlPts));
+
+  auto nkeys = keys2midverts.size();
+
+
+  new_mesh->add_tag<Real>(1, "bezier_pts", n_edge_pts*dim);
+  new_mesh->add_tag<Real>(0, "bezier_pts", dim);
+  new_mesh->set_tag_for_ctrlPts(1, Reals(edge_ctrlPts));
+
+  return LOs(keys2old_faces_w);
+}
+
+#define OMEGA_H_INST(T)
+OMEGA_H_INST(I8)
+OMEGA_H_INST(I32)
+OMEGA_H_INST(I64)
+OMEGA_H_INST(Real)
+#undef OMEGA_H_INST
+
+} // namespace Omega_h
