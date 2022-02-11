cb8e1806d1996ad9898bd0bcf93b9af635f7afa1
joshia5 <joshia5@rpi.edu>
2022-02-10 22:03:22 -0500

    add unit tri validity test

--- src/curve_coarsen_test.cpp
+++ src/curve_coarsen_test.cpp
@@ -5,45 +5,111 @@
 #include <Omega_h_metric.hpp>
 #include <Omega_h_file.hpp>
 #include <Omega_h_beziers.hpp>
-
-#include <cmath>
+#include <Omega_h_for.hpp>
 
 using namespace Omega_h;
 
-int main(int argc, char** argv) {
-  auto lib = Library(&argc, &argv);
-  auto comm = lib.world();
+void test_tri_validity(Library *lib) {
+  auto mesh = Mesh(lib);
+  binary::read(
+      "/lore/joshia5/develop/mfem_omega/omega_h/meshes/Example_tri.osh",
+      lib->world(), &mesh);
+  mesh.set_curved(1);
+  mesh.set_max_order(3);
+  mesh.add_tags_for_ctrlPts();
+  auto dim = mesh.dim();
+  auto coords = mesh.coords();
+  auto edge_nCtrlPts = mesh.n_internal_ctrlPts(1);
+  auto ev2v = mesh.ask_down(1, 0).ab2b;
 
-  auto mesh = meshsim::read("/users/joshia5/Meshes/curved/disk_semi_100_order2.sms",
+  mesh.add_tag<Real>(0, "bezier_pts", dim, coords);
+  mesh.set_tag_for_ctrlPts(1, Reals({1.0/3.0,0.0, 2.0/3.0,0.0, 
+                                     2.0/3.0,1.0/3.0, 1.0/3.0,2.0/3.0,
+                                     0.0,2.0/3.0, 0.0,1.0/3.0}));
+  mesh.set_tag_for_ctrlPts(2, Reals({1.0/3.0, 1.0/3.0}));
+  auto valid_tris = checkValidity_2d(&mesh, LOs(mesh.nfaces(), 0, 1));
+  return;
+}
+
+void test_disc_validity(Library *lib) {
+  auto comm = lib->world();
+
+  auto mesh = meshsim::read("/users/joshia5/Meshes/curved/disk_semi_2tri_order2.sms",
+                          "/users/joshia5/Models/curved/disk_semi_geomsim.smd", comm);
+  /*auto mesh = meshsim::read("/users/joshia5/Meshes/curved/disk_semi_100_order2.sms",
                             "/users/joshia5/Models/curved/disk_semi_geomsim_100.smd", lib.world());
+                            */
   calc_quad_ctrlPts_from_interpPts(&mesh);
   elevate_curve_order_2to3(&mesh);
   mesh.add_tag<Real>(0, "bezier_pts", mesh.dim(), mesh.coords());
-  auto wireframe_mesh = Mesh(&lib);
+  auto wireframe_mesh = Mesh(lib);
   wireframe_mesh.set_comm(comm);
   build_cubic_wireframe_2d(&mesh, &wireframe_mesh, 4);
-  std::string vtuPath = "/users/joshia5/Meshes/curved/disc100_cubic_wireframe.vtu";
+  std::string vtuPath =
+    "/users/joshia5/Meshes/curved/disc2_cubic_wireframe.vtu";
+    //"/users/joshia5/Meshes/curved/disc100_cubic_wireframe.vtu";
   vtk::write_simplex_connectivity(vtuPath.c_str(), &wireframe_mesh, 1);
-  auto cubic_curveVtk_mesh = Mesh(&lib);
+  auto cubic_curveVtk_mesh = Mesh(lib);
   cubic_curveVtk_mesh.set_comm(comm);
   build_cubic_curveVtk_2d(&mesh, &cubic_curveVtk_mesh, 4);
-  vtuPath = "/users/joshia5/Meshes/curved/disc100_cubic_curveVtk.vtu";
+  vtuPath = "/users/joshia5/Meshes/curved/disc2_cubic_curveVtk.vtu";
+  //vtuPath = "/users/joshia5/Meshes/curved/disc100_cubic_curveVtk.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &cubic_curveVtk_mesh, 2);
+
+  auto valid_tris = checkValidity_2d(&mesh, LOs(mesh.nfaces(), 0, 1));
+  return;
+}
+
+void test_disc_collapse(Library *lib) {
+  auto comm = lib->world();
+
+  auto mesh = meshsim::read("/users/joshia5/Meshes/curved/disk_semi_2tri_order2.sms",
+                          "/users/joshia5/Models/curved/disk_semi_geomsim.smd", comm);
+  /*auto mesh = meshsim::read("/users/joshia5/Meshes/curved/disk_semi_100_order2.sms",
+                            "/users/joshia5/Models/curved/disk_semi_geomsim_100.smd", lib.world());
+                            */
+  calc_quad_ctrlPts_from_interpPts(&mesh);
+  elevate_curve_order_2to3(&mesh);
+  mesh.add_tag<Real>(0, "bezier_pts", mesh.dim(), mesh.coords());
+  auto wireframe_mesh = Mesh(lib);
+  wireframe_mesh.set_comm(comm);
+  build_cubic_wireframe_2d(&mesh, &wireframe_mesh, 4);
+  std::string vtuPath =
+    "/users/joshia5/Meshes/curved/disc2_cubic_wireframe.vtu";
+    //"/users/joshia5/Meshes/curved/disc100_cubic_wireframe.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &wireframe_mesh, 1);
+  auto cubic_curveVtk_mesh = Mesh(lib);
+  cubic_curveVtk_mesh.set_comm(comm);
+  build_cubic_curveVtk_2d(&mesh, &cubic_curveVtk_mesh, 4);
+  vtuPath = "/users/joshia5/Meshes/curved/disc2_cubic_curveVtk.vtu";
+  //vtuPath = "/users/joshia5/Meshes/curved/disc100_cubic_curveVtk.vtu";
   vtk::write_simplex_connectivity(vtuPath.c_str(), &cubic_curveVtk_mesh, 2);
 
   auto opts = AdaptOpts(&mesh);
   mesh.add_tag<Real>(VERT, "metric", 1);
   mesh.set_tag(
-      VERT, "metric", Reals(mesh.nverts(), metric_eigenvalue_from_length(0.3)));
+      VERT, "metric", Reals(mesh.nverts(),
+        metric_eigenvalue_from_length(0.3)));
+  while (coarsen_by_size(&mesh, opts));
+  mesh.set_tag(
+      VERT, "metric", Reals(mesh.nverts(),
+        metric_eigenvalue_from_length(0.6)));
   while (coarsen_by_size(&mesh, opts))
     ;
   mesh.set_tag(
-      VERT, "metric", Reals(mesh.nverts(), metric_eigenvalue_from_length(0.6)));
-  while (coarsen_by_size(&mesh, opts))
-    ;
-  mesh.set_tag(
-      VERT, "metric", Reals(mesh.nverts(), metric_eigenvalue_from_length(1.0)));
+      VERT, "metric", Reals(mesh.nverts(),
+        metric_eigenvalue_from_length(1.0)));
   while (coarsen_by_size(&mesh, opts))
     ;
   mesh.ask_qualities();
+  return;
+}
+
+int main(int argc, char** argv) {
+  auto lib = Library(&argc, &argv);
+
+  //test_disc_collapse(&lib);
+  //test_disc_validity(&lib);
+  test_tri_validity(&lib);
   return 0;
 }
