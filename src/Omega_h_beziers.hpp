d3fa95f72d7cc01fc530d22c7c3faad26ad8d7bb
joshia5 <joshia5@rpi.edu>
2022-02-18 10:52:35 -0500

    working validity check for cand new tris

--- src/Omega_h_beziers.hpp
+++ src/Omega_h_beziers.hpp
@@ -1178,6 +1178,102 @@ LOs coarsen_curved_verts_and_edges_2d(Mesh *mesh, Mesh *new_mesh,
 
 LOs checkValidity(Mesh *new_mesh, LOs new_tris, Int const mesh_dim);
 
+OMEGA_H_INLINE LO computeTriNodeIndex (LO P, LO i, LO j) {
+  LO k = P-i-j;
+  if (i == P) return 0;
+  if (j == P) return 1;
+  if (k == P) return 2;
+  if (k == 0) return 2+j;
+  if (i == 0) return 2+(P-1)+k;
+  if (j == 0) return 2+(P-1)*2+i;
+  return k*(P-1)-k*(k-1)/2+j+2*P;
+}
+
+OMEGA_H_INLINE LO getTriNodeIndex (LO P, LO i, LO j) {
+  return computeTriNodeIndex(P,i,j);
+}
+
+template <Int n>
+OMEGA_H_INLINE Real getTriPartialJacobianDet(Few<Real, 20> nodes,
+  LO P, LO i1, LO j1, LO i2, LO j2) {
+  LO p00 = getTriNodeIndex(P,i1+1,j1);
+  LO p01 = getTriNodeIndex(P,i1,j1+1);
+  LO p10 = getTriNodeIndex(P,i2+1,j2);
+  LO p11 = getTriNodeIndex(P,i2,j2);
+  return cross(get_vector<n>(nodes, p01) - get_vector<n>(nodes, p00),
+               get_vector<n>(nodes, p11) - get_vector<n>(nodes, p10));
+  /*the return will be following for 3d
+  return cross(get_vector<n>(nodes, p01) - get_vector<n>(nodes, p00),
+               get_vector<n>(nodes, p11) - get_vector<n>(nodes, p10))[2];
+  */
+}
+
+OMEGA_H_INLINE Real Nijk(Few<Real, 20> nodes, LO d, LO I, LO J) {
+  Real sum = 0.;
+  LO CD = trinomial(2*(d-1), I, J);
+  for (LO j1 = 0; j1 <= J; ++j1) {
+    auto i1start = max2(0, I+J-j1-(d-1));
+    auto i1end = min2(I, d-1-j1);
+    for (LO i1 = i1start; i1 <= i1end; ++i1){
+      sum += trinomial(d-1, i1, j1)*trinomial(d-1, I-i1, J-j1)
+        *getTriPartialJacobianDet<2>(nodes, d, i1, j1, I-i1, J-j1);
+    }
+  }
+  return sum*d*d/CD;
+}
+
+template <Int n>
+OMEGA_H_INLINE Few<Real, n> getTriJacDetNodes(
+    LO P, Few<Real, 20> const& elemNodes) {
+  Few<Real, n> nodes;//n=15
+  for (LO I = 0; I <= 2*(P-1); ++I) {
+    for (LO J = 0; J <= 2*(P-1)-I; ++J) {
+        OMEGA_H_CHECK(getTriNodeIndex(2*(P-1),I,J) < nodes.size());
+        nodes[getTriNodeIndex(2*(P-1),I,J)] = Nijk(elemNodes,P,I,J);
+    }
+  }
+  return nodes;
+}
+
+template<Int n>
+OMEGA_H_INLINE LO checkMinJacDet(Few<Real, n> const& nodes) {
+  // first 3 vertices
+  Real minAcceptable = 0.0;
+  for (LO i = 0; i < 3; ++i) {
+    printf("i %d Nijk[i] %f\n", i, nodes[i]);
+    if (nodes[i] < minAcceptable) {
+      return i+2;
+    }
+  }
+
+  /*
+  Real minJ = 0;
+  for (LO edge = 0; edge < 3; ++edge) {
+    for (LO i = 0; i < 2*(order-1)-1; ++i) {
+      if (nodes[3+edge*(2*(order-1)-1)+i] < minAcceptable) {
+        minJ = -1e10;
+        // there is no point in doing much with edges if we dont have
+        // elevation or subdivision or subdivision matrices
+        if (minJ < minAcceptable){
+          return 8+edge;
+        }
+      }
+    }
+  }
+
+  for (LO i = 0; i < (2*order-3)*(2*order-4)/2; ++i) {
+    if (nodes[6*(order-1)+i] < minAcceptable) {
+      minJ = -1e10;
+      if (minJ < minAcceptable) {
+        return 14;
+      }
+    }
+  }
+  */
+  return -1;
+}
+
+//TODO template dim, nfacePts
 } // namespace Omega_h
 
 #endif
