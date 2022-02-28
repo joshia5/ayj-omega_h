c43423a8c5ffc30fa9e1988b6cd6363749c58f5b
joshia5 <joshia5@rpi.edu>
2022-02-28 17:59:31 -0500

    fix return type mismatch and cleanup

--- src/Omega_h_curve_validity_3d.hpp
+++ src/Omega_h_curve_validity_3d.hpp
@@ -8,7 +8,6 @@ namespace Omega_h {
 
 OMEGA_H_INLINE LO computeTetNodeIndex (LO P, LO i, LO j, LO k) {
   LO l = P-i-j-k;
-  int l = P-i-j-k;
   if(i == P) return 0;
   if(j == P) return 1;
   if(k == P) return 2;
@@ -43,7 +42,7 @@ OMEGA_H_INLINE Real getTetPartialJacobianDet(Few<Real, 60> nodes, LO P, LO i1,
          *(get_vector<3>(nodes, p21) - get_vector<3>(nodes, p20));
 }
 
-OMEGA_H_INLINE Real Nijkl(Few<Real, 60> nodes, LO d, LO I, LO J, LO k) {
+OMEGA_H_INLINE Real Nijkl(Few<Real, 60> nodes, LO d, LO I, LO J, LO K) {
 
   Real sum = 0.;
   LO CD = quadnomial(3*(d-1),I,J,K);
@@ -75,7 +74,7 @@ OMEGA_H_INLINE Real Nijkl(Few<Real, 60> nodes, LO d, LO I, LO J, LO k) {
 }
 
 template <Int n>
-OMEGA_H_INLINE void getTetJacDetNodes(LO P, Few<Real, 60> const& elemNodes) {
+OMEGA_H_INLINE Few<Real, n> getTetJacDetNodes(LO P, Few<Real, 60> const& elemNodes) {
   Few<Real, n> nodes;//n=84
   for (int I = 0; I <= 3*(P-1); ++I) {
     for (int J = 0; J <= 3*(P-1)-I; ++J) {
@@ -88,11 +87,11 @@ OMEGA_H_INLINE void getTetJacDetNodes(LO P, Few<Real, 60> const& elemNodes) {
   return nodes;
 }
 
-OMEGA_H_INLINE LO checkMinJacDet_3d(Few<Real, 60> const& nodes) {
+OMEGA_H_INLINE LO checkMinJacDet_3d(Few<Real, 84> const& nodes) {
   // first 4 vertices
   Real minAcceptable = 0.0;
   for (LO i = 0; i < 4; ++i) {
-    printf("i %d Nijk[i] %f\n", i, nodes[i]);
+    printf("i %d Nijkl[i] %f\n", i, nodes[i]);
     if (nodes[i] < minAcceptable) {
       return i+2;
     }
@@ -125,7 +124,8 @@ OMEGA_H_INLINE LO checkMinJacDet_3d(Few<Real, 60> const& nodes) {
   return -1;
 }
 
-LOs checkValidity_3d(Mesh *mesh, LOs new_tris, Int const mesh_dim) {
+/*
+void checkValidity_3d(Mesh *mesh, LOs new_tris, Int const mesh_dim) {
   auto fv2v = mesh->ask_down(2, 0).ab2b;
   auto fe2e = mesh->get_adj(2, 1).ab2b;
   auto ev2v = mesh->get_adj(1, 0).ab2b;
@@ -136,100 +136,92 @@ LOs checkValidity_3d(Mesh *mesh, LOs new_tris, Int const mesh_dim) {
   auto order = mesh->get_max_order();
   OMEGA_H_CHECK(order == 3);
 
-  Write<LO> is_invalid(new_tris.size(), -1);
-  //LO const ntri_pts = 10;
+  Few<Real, 60> tet_pts;//ntet_pts*dim=20
 
-  auto check_validity = OMEGA_H_LAMBDA (LO n) {
-    //TODO change tri_pts size to 30 for 3d
-    Few<Real, 20> tri_pts;//ntri_pts*dim=20
-    auto tri = new_tris[n];
-
-    //query the tri's down verts's ctrl pts and store
-    for (LO j = 0; j < 3; ++j) {
-      if (mesh_dim == 2) {
-        auto p = get_vector<2>(vertCtrlPts, fv2v[tri*3 + j]);
-        for (LO k = 0; k < mesh_dim; ++k) {
-          tri_pts[j*mesh_dim + k] = p[k];
-        }
+  //query the tri's down verts's ctrl pts and store
+  for (LO j = 0; j < 3; ++j) {
+    if (mesh_dim == 2) {
+      auto p = get_vector<2>(vertCtrlPts, fv2v[tri*3 + j]);
+      for (LO k = 0; k < mesh_dim; ++k) {
+        tri_pts[j*mesh_dim + k] = p[k];
       }
-      else {
-        OMEGA_H_CHECK (mesh_dim == 3);
-        auto p = get_vector<3>(vertCtrlPts, fv2v[tri*3 + j]);
-        for (LO k = 0; k < mesh_dim; ++k) {
-          tri_pts[j*mesh_dim + k] = p[k];
-        }
-      }
-    }
-
-    //query the tri's down edge's ctrl pts and store
-
-    auto v0 = fv2v[tri*3 + 0];
-    auto v1 = fv2v[tri*3 + 1];
-    auto v2 = fv2v[tri*3 + 2];
-    auto e0 = fe2e[tri*3 + 0];
-    auto e1 = fe2e[tri*3 + 1];
-    auto e2 = fe2e[tri*3 + 2];
-    auto e0v0 = ev2v[e0*2 + 0];
-    auto e0v1 = ev2v[e0*2 + 1];
-    auto e1v0 = ev2v[e1*2 + 0];
-    auto e1v1 = ev2v[e1*2 + 1];
-    auto e2v0 = ev2v[e2*2 + 0];
-    auto e2v1 = ev2v[e2*2 + 1];
-    auto flip = vector_3(-1, -1, -1);
-    if ((e0v0 == v1) && (e0v1 == v0)) {
-      flip[0] = 1;
     }
     else {
-      OMEGA_H_CHECK((e0v0 == v0) && (e0v1 == v1));
+      OMEGA_H_CHECK (mesh_dim == 3);
+      auto p = get_vector<3>(vertCtrlPts, fv2v[tri*3 + j]);
+      for (LO k = 0; k < mesh_dim; ++k) {
+        tri_pts[j*mesh_dim + k] = p[k];
+      }
     }
-    if ((e1v0 == v2) && (e1v1 == v1)) {
-      flip[1] = 1;
+  }
+
+  //query the tri's down edge's ctrl pts and store
+
+  auto v0 = fv2v[tri*3 + 0];
+  auto v1 = fv2v[tri*3 + 1];
+  auto v2 = fv2v[tri*3 + 2];
+  auto e0 = fe2e[tri*3 + 0];
+  auto e1 = fe2e[tri*3 + 1];
+  auto e2 = fe2e[tri*3 + 2];
+  auto e0v0 = ev2v[e0*2 + 0];
+  auto e0v1 = ev2v[e0*2 + 1];
+  auto e1v0 = ev2v[e1*2 + 0];
+  auto e1v1 = ev2v[e1*2 + 1];
+  auto e2v0 = ev2v[e2*2 + 0];
+  auto e2v1 = ev2v[e2*2 + 1];
+  auto flip = vector_3(-1, -1, -1);
+  if ((e0v0 == v1) && (e0v1 == v0)) {
+    flip[0] = 1;
+  }
+  else {
+    OMEGA_H_CHECK((e0v0 == v0) && (e0v1 == v1));
+  }
+  if ((e1v0 == v2) && (e1v1 == v1)) {
+    flip[1] = 1;
+  }
+  else {
+    OMEGA_H_CHECK((e1v0 == v1) && (e1v1 == v2));
+  }
+  if ((e2v0 == v0) && (e2v1 == v2)) {
+    flip[2] = 1;
+  }
+
+  for (LO j = 0; j < 3; ++j) {
+    LO index = 3;
+    if (flip[j] == -1) {
+      for (I8 d = 0; d < mesh_dim; ++d) {
+        tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + d] =
+          edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + d];
+        tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + mesh_dim + d] =
+          edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + mesh_dim + d];
+      }
     }
     else {
-      OMEGA_H_CHECK((e1v0 == v1) && (e1v1 == v2));
-    }
-    if ((e2v0 == v0) && (e2v1 == v2)) {
-      flip[2] = 1;
-    }
-
-    for (LO j = 0; j < 3; ++j) {
-      LO index = 3;
-      if (flip[j] == -1) {
-        for (I8 d = 0; d < mesh_dim; ++d) {
-          tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + d] =
-            edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + d];
-          tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + mesh_dim + d] =
-            edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + mesh_dim + d];
-        }
-      }
-      else {
-        //for flipped edges
-        OMEGA_H_CHECK (flip[j] == 1);
-        for (I8 d = 0; d < mesh_dim; ++d) {
-          tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + d] =
-            edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + mesh_dim + d];
-          tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + mesh_dim + d] =
-            edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + d];
-        }
+      //for flipped edges
+      OMEGA_H_CHECK (flip[j] == 1);
+      for (I8 d = 0; d < mesh_dim; ++d) {
+        tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + d] =
+          edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + mesh_dim + d];
+        tri_pts[index*mesh_dim + j*n_edge_pts*mesh_dim + mesh_dim + d] =
+          edgeCtrlPts[fe2e[tri*3 + j]*n_edge_pts*mesh_dim + d];
       }
     }
+  }
 
-    //query the face's ctrl pt and store
-    for (I8 d = 0; d < mesh_dim; ++d) {
-      LO index = 9;
-      tri_pts[index*mesh_dim + d] = faceCtrlPts[tri*mesh_dim + d];
-    }
+  //query the face's ctrl pt and store
+  for (I8 d = 0; d < mesh_dim; ++d) {
+    LO index = 9;
+    tri_pts[index*mesh_dim + d] = faceCtrlPts[tri*mesh_dim + d];
+  }
 
-    //TODO change to template for mesh_dim
-    auto nodes_det = getTriJacDetNodes<15, 2>(order, tri_pts);
+  //TODO change to template for mesh_dim
+  auto nodes_det = getTriJacDetNodes<15, 2>(order, tri_pts);
 
-    is_invalid[n] = checkMinJacDet<15>(nodes_det);
-    if (is_invalid[n] > 0) printf("invalid tri %d\n", tri);
-  };
-  parallel_for(new_tris.size(), std::move(check_validity));
+  is_invalid[n] = checkMinJacDet<15>(nodes_det);
+  if (is_invalid[n] > 0) printf("invalid tri %d\n", tri);
 
-  return LOs(is_invalid);
+  return;
 }
-  */
+*/
 
 } //namespace
