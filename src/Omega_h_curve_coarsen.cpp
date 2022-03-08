a4079826a09fbb3097b4ce07dde37dff5e6e5642
joshia5 <joshia5@rpi.edu>
2022-03-08 13:55:13 -0500

    add fn to check invalid tets after curving boundaries

---
+++ src/Omega_h_curve_coarsen.cpp
@@ -0,0 +1,67 @@
+#include "Omega_h_curve_coarsen.hpp"
+
+#include "Omega_h_curve_validity_3d.hpp"
+
+namespace Omega_h {
+
+void correct_curved_edges(Mesh *new_mesh) {
+
+  auto const edge_crvtoBdryFace = new_mesh->get_array<I8>(1, "edge_crvtoBdryFace");
+  auto const new_rv2v = new_mesh->ask_down(3, 0).ab2b;
+  auto const new_re2e = new_mesh->ask_down(3, 1).ab2b;
+  auto const new_rf2f = new_mesh->ask_down(3, 2).ab2b;
+  auto const new_ev2v = new_mesh->ask_down(1, 0).ab2b;
+  auto const new_e2r = new_mesh->ask_up(1, 3);
+  auto const new_e2er = new_e2r.a2ab;
+  auto const new_er2r = new_e2r.ab2b;
+  auto const nnew_edge = new_mesh->nedges();
+  auto const n_edge_pts = new_mesh->n_internal_ctrlPts(1);
+  auto const dim = new_mesh->dim();
+  auto const new_coords = new_mesh->coords();
+
+  auto const vertCtrlPts = new_mesh->get_ctrlPts(0);
+  auto const edgeCtrlPts = new_mesh->get_ctrlPts(1);
+  auto const faceCtrlPts = new_mesh->get_ctrlPts(2);
+  Write<Real> edge_ctrlPts_new(nnew_edge*n_edge_pts*dim, INT8_MAX);
+
+  auto edge_correct = OMEGA_H_LAMBDA(LO i) {
+    LO has_invalid_tet = -1;
+    if (edge_crvtoBdryFace[i] == 1) {
+      for (LO er = new_e2er[i]; er < new_e2er[i+1]; ++er) {
+        if (has_invalid_tet > 0) break;
+        LO adj_tet = new_er2r[er];
+        Few<Real, 60> tet_pts = collect_tet_pts(3, adj_tet, new_ev2v, new_rv2v, vertCtrlPts
+            , edgeCtrlPts, faceCtrlPts, new_re2e, new_rf2f);
+
+        Few<Real, 84> nodes_det = getTetJacDetNodes<84>(3, tet_pts);
+
+        has_invalid_tet = checkMinJacDet_3d(nodes_det);
+        if (has_invalid_tet) {
+          printf(" correcting invalid tet %d\n", adj_tet);
+          auto v0 = new_ev2v[i*2 + 0];
+          auto v1 = new_ev2v[i*2 + 1];
+          for (LO j=0; j<dim; ++j) {
+            edge_ctrlPts_new[i*n_edge_pts*dim + j] = new_coords[v0*dim + j] +
+              (new_coords[v1*dim + j] - new_coords[v0*dim + j])*(1.0/3.0);
+            edge_ctrlPts_new[i*n_edge_pts*dim + dim + j] = new_coords[v0*dim + j] +
+              (new_coords[v1*dim + j] - new_coords[v0*dim + j])*(2.0/3.0);
+          }
+        }
+      }
+    }
+    else {
+      for (LO j = 0; j<n_edge_pts*dim; ++j) {
+        edge_ctrlPts_new[i*n_edge_pts*dim + j] = edgeCtrlPts[i*n_edge_pts*dim + j];
+      }
+    }
+  };
+  parallel_for(nnew_edge, std::move(edge_correct), "edge_correct");
+  new_mesh->set_tag_for_ctrlPts(1, Reals(edge_ctrlPts_new));
+  //pfor over all edges
+  //if it was curved before, check validity of all its adjacent tets
+  //  if any adjacent tet is invalid, straighten this edge
+  //else keep pts as is
+  return;
+}
+
+} // namespace Omega_h
--- src/Omega_h_curve_coarsen.hpp
+++ src/Omega_h_curve_coarsen.hpp
@@ -1,3 +1,6 @@
+#ifndef OMEGA_H_CURVE_COARSEN_HPP
+#define OMEGA_H_CURVE_COARSEN_HPP
+
 #include "Omega_h_mesh.hpp"
 #include "Omega_h_beziers.hpp"
 #include "Omega_h_element.hpp"
@@ -24,15 +27,16 @@ void coarsen_curved_verts_and_edges(Mesh *mesh, Mesh *new_mesh, LOs old2new,
   }
   auto const old_vertCtrlPts = mesh->get_ctrlPts(0);
   auto const old_edgeCtrlPts = mesh->get_ctrlPts(1);
-  auto const n_edge_pts = mesh->n_internal_ctrlPts(1);
   auto const old_coords = mesh->coords();
 
   auto const new_ev2v = new_mesh->get_adj(1, 0).ab2b;
   auto const new_coords = new_mesh->coords();
   auto const nnew_edge = new_mesh->nedges();
+  auto const n_edge_pts = mesh->n_internal_ctrlPts(1);
   auto const nnew_verts = new_mesh->nverts();
 
   Write<Real> edge_ctrlPts(nnew_edge*n_edge_pts*dim, INT8_MAX);
+  Write<I8> edge_crvtoBdryFace(nnew_edge, -1);
   Write<Real> vert_ctrlPts(nnew_verts*1*dim, INT8_MAX);
 
   //copy ctrl pts for verts
@@ -68,22 +72,20 @@ void coarsen_curved_verts_and_edges(Mesh *mesh, Mesh *new_mesh, LOs old2new,
     for (LO j=0; j<dim; ++j) {
       edge_ctrlPts[e*n_edge_pts*dim + j] = new_coords[v0*dim + j] +
           (new_coords[v1*dim + j] - new_coords[v0*dim + j])*(1.0/3.0);
-          //TODO verify that new pts should be equidistant
-          //(new_coords[v1*dim + j] - new_coords[v0*dim + j])*xi_1_cube();
       edge_ctrlPts[e*n_edge_pts*dim + dim + j] = new_coords[v0*dim + j] +
           (new_coords[v1*dim + j] - new_coords[v0*dim + j])*(2.0/3.0);
+          //TODO verify that new pts should be equidistant
+          //(new_coords[v1*dim + j] - new_coords[v0*dim + j])*xi_1_cube();
           //(new_coords[v1*dim + j] - new_coords[v0*dim + j])*xi_2_cube();
     }
   };
   parallel_for(prods2new.size(), std::move(prod_edge_points),
       "prod_edge_points");
 
-  auto newvert_gdim =  new_mesh->get_array<I8>(0, "class_dim");
   auto newedge_gdim =  new_mesh->get_array<I8>(1, "class_dim");
   auto newedge_gid =  new_mesh->get_array<LO>(1, "class_id");
   auto oldvert_gdim =  mesh->get_array<I8>(0, "class_dim");
   auto oldvert_gid =  mesh->get_array<LO>(0, "class_id");
-  auto oldedge_gid =  mesh->get_array<LO>(1, "class_id");
 
   auto v2v_old = mesh->ask_star(0);
   auto v2vv_old = v2v_old.a2ab;
@@ -106,39 +108,11 @@ void coarsen_curved_verts_and_edges(Mesh *mesh, Mesh *new_mesh, LOs old2new,
         if ((newedge_gdim[new_edge] <= (dim-1))) {
         //if ((newedge_gdim[new_edge] <= (dim-1)) && (newedge_gdim[new_edge] == oldvert_gdim[v_key])
           //  && (newedge_gid[new_edge] == oldvert_gid[v_key])) {
+          if (newedge_gdim[new_edge] == 2) {
+            edge_crvtoBdryFace[new_edge] = 1;
+          }
           auto new_edge_v0 = new_ev2v[new_edge*2 + 0];
           auto new_edge_v1 = new_ev2v[new_edge*2 + 1];
-          /*
-          //TODO use 2 pts on 2 collapsing for better curvature
-          LO new_edge_old_edge0 = -1;
-          LO new_edge_old_edge1 = -1;
-          for (LO ve = old_v2ve[v_key]; ve < old_v2ve[v_key+1]; ++ve) {
-            auto old_edge = old_ve2e[ve];
-            auto old_edge_v0 = old_ev2v[old_edge*2 + 0];
-            auto old_edge_v1 = old_ev2v[old_edge*2 + 1];
-            LO use_v_old = -1;
-            assert((old_edge_v0 == v_key) || (old_edge_v1 == v_key)); 
-            if (old_edge_v1 == v_key) {
-              use_v_old = old_edge_v0;
-            }
-            else {
-              assert (old_edge_v0 == v_key);
-              use_v_old = old_edge_v1;
-            }
-
-            if (old_verts2new_verts[use_v_old] == new_edge_v0) {
-              new_edge_old_edge0 = old_edge;
-            }
-            else if (old_verts2new_verts[use_v_old] == new_edge_v1) {
-              new_edge_old_edge1 = old_edge;
-            }
-            else {}
-          }
-          auto new_old_e0_v0 = old_ev2v[new_edge_old_edge0*2 + 0];
-          auto new_old_e0_v1 = old_ev2v[new_edge_old_edge0*2 + 1];
-          auto new_old_e1_v0 = old_ev2v[new_edge_old_edge1*2 + 0];
-          auto new_old_e1_v1 = old_ev2v[new_edge_old_edge1*2 + 1];
-          */
 
           auto c0 = get_vector<dim>(Reals(vert_ctrlPts), new_edge_v0);
           auto c3 = get_vector<dim>(Reals(vert_ctrlPts), new_edge_v1);
@@ -175,7 +149,7 @@ void coarsen_curved_verts_and_edges(Mesh *mesh, Mesh *new_mesh, LOs old2new,
   parallel_for(keys2verts.size(), std::move(find_bdry_edges));
 
   new_mesh->add_tag<Real>(1, "bezier_pts", n_edge_pts*dim, Reals(edge_ctrlPts));
-  new_mesh->set_tag_for_ctrlPts(1, Reals(edge_ctrlPts));
+  new_mesh->add_tag<I8>(1, "edge_crvtoBdryFace", 1, Read<I8>(edge_crvtoBdryFace));
 
   return;
 }
@@ -239,6 +213,7 @@ void coarsen_curved_faces(Mesh *mesh, Mesh *new_mesh, LOs old2new,
 
   return;
 }
+void correct_curved_edges(Mesh *new_mesh);
 
 #define OMEGA_H_INST(T)                                                       
 OMEGA_H_INST(I8)
@@ -248,3 +223,5 @@ OMEGA_H_INST(Real)
 #undef OMEGA_H_INST
 
 } // namespace Omega_h
+
+#endif
