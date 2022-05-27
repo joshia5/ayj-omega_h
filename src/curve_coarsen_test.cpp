0a6603d79dc2ef80a20f53eb4ff0c05a06abb70d
joshia5 <joshia5@rpi.edu>
2022-05-27 13:55:59 -0400

    run kova test

--- src/curve_coarsen_test.cpp
+++ src/curve_coarsen_test.cpp
@@ -436,8 +436,8 @@ int main(int argc, char** argv) {
   //test_quadratic_tet_validity(&lib);
   //test_Kova_validity(&lib);
   //test_cubic_tet_validity(&lib);
-  //test_collapse_kova(&lib);
-  test_collapse_boxCircle(&lib);
+  test_collapse_kova(&lib);
+  //test_collapse_boxCircle(&lib);
   //test_collapse_cubicSlab(&lib);
   //test_antenna(&lib);
 
