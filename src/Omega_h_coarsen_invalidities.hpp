9c4fe0be57798387594095e3c23b5b6e7ecdd8c9
joshia5 <joshia5@rpi.edu>
2022-08-23 12:22:35 -0400

    allow cands with select v_onto

--- src/Omega_h_coarsen_invalidities.hpp
+++ src/Omega_h_coarsen_invalidities.hpp
@@ -11,7 +11,7 @@ LOs coarsen_invalidities(Mesh* mesh, LOs cands2edges, Read<I8> cand_codes);
 
 LOs coarsen_invalidities_new_mesh(Mesh* mesh, LOs cands2edges,
   Read<I8> cand_codes, Mesh* new_mesh, LOs const old_verts2new_verts,
-  Read<I8> const verts_are_keys);
+  Read<I8> const verts_are_keys, LOs const keys2verts_onto);
 
 }  // end namespace Omega_h
 
