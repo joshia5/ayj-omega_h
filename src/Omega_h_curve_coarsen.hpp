82ac3443dc0ea59b52819a1b34cc4a8b4c6d83ab
joshia5 <joshia5@rpi.edu>
2022-09-28 10:10:22 -0400

    pass keys2verts

--- src/Omega_h_curve_coarsen.hpp
+++ src/Omega_h_curve_coarsen.hpp
@@ -1457,7 +1457,8 @@ void coarsen_curved_faces(Mesh *mesh, Mesh *new_mesh, const LOs old2new,
 void check_validity_all_tet(Mesh *new_mesh);
 void correct_curved_edges(Mesh *new_mesh);
 void check_validity_new_curved_edges(Mesh *new_mesh);
-void check_validity_edges_from_complex_cav(Mesh *new_mesh);
+void check_validity_edges_from_complex_cav(Mesh *new_mesh, LOs const
+  keys2verts);
 
 #define OMEGA_H_INST(T)                                                       
 OMEGA_H_INST(I8)
