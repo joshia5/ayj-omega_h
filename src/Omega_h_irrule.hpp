8b90bfaab7f7f55a5b95ff5c72d672f86c5f6805
joshia5 <joshia5@rpi.edu>
2022-10-04 13:05:48 -0400

    remove mfem fns

--- src/Omega_h_irrule.hpp
+++ src/Omega_h_irrule.hpp
@@ -9,50 +9,10 @@
 #include <fstream>
 
 namespace Omega_h {
-/// Class for integration point with weight
-   Real x, y, z, weight;
-   LO index;
 
-   void Init(LO const i)
-   {
-      x = y = z = weight = 0.0;
-      index = i;
-   }
-}
-
-OMEGA_H_DEVICE void TetGaussLobatto(const int order = 4) {
-
+OMEGA_H_DEVICE Few<Real, 44> void TetGaussLobatto(const int order = 4) {
   OMEGA_H_CHECK(order == 4);
-  IntegrationRule *ir;
-  // 11 points - degree 4 (negative weight)
-  ir = new IntegrationRule(11);
   Few<Real, 11*4> ir //11 points, store x,y,z,weights
-
-  void Set(const double x1, const double x2, const double x3, const double w)
-   { x = x1; y = x2; z = x3; weight = w; }
-
-  // given a, add the permutations of (a,a,a,b), where 3*a + b = 1
-   void AddTetPoints4(const int off, const double a, const double weight)
-   {
-      IntPoint(off).Set(a, a, a, weight);
-      AddTetPoints3(off + 1, a, 1. - 3.*a, weight); }
-   // add the permutations of (a,a,b)
-   void AddTetPoints3(const int off, const double a, const double b,
-                      const double weight)
-   {
-      IntPoint(off + 0).Set(a, a, b, weight);
-      IntPoint(off + 1).Set(a, b, a, weight);
-      IntPoint(off + 2).Set(b, a, a, weight);
-   }
-  // add the permutations of (a,a,b,b), 2*(a + b) = 1
-  void AddTetPoints6(const int off, const double a, const double weight)
-  {
-      const double b = 0.5 - a;
-      AddTetPoints3(off,     a, b, weight);
-      AddTetPoints3(off + 3, b, a, weight);
-  }
-  void AddTetMidPoint(const int off, const double weight)
-   { IntPoint(off).Set(0.25, 0.25, 0.25, weight); }
 
   //AddTetPoints4(0, 1./14., 343./45000.);
   { 
