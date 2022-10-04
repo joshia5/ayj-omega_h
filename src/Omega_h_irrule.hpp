22868c5d53e4855506fbe02d39638958c2909ad6
joshia5 <joshia5@rpi.edu>
2022-10-04 13:03:55 -0400

    set points and weights

--- src/Omega_h_irrule.hpp
+++ src/Omega_h_irrule.hpp
@@ -2,6 +2,8 @@
 #define OMEGA_H_IRRULE_HPP
 
 #include "Omega_h_array.hpp"
+#include "Omega_h_array_ops.hpp"
+#include "Omega_h_few.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -28,7 +30,13 @@ OMEGA_H_DEVICE void TetGaussLobatto(const int order = 4) {
 
   void Set(const double x1, const double x2, const double x3, const double w)
    { x = x1; y = x2; z = x3; weight = w; }
-  // add the permutations of (a,a,b)
+
+  // given a, add the permutations of (a,a,a,b), where 3*a + b = 1
+   void AddTetPoints4(const int off, const double a, const double weight)
+   {
+      IntPoint(off).Set(a, a, a, weight);
+      AddTetPoints3(off + 1, a, 1. - 3.*a, weight); }
+   // add the permutations of (a,a,b)
    void AddTetPoints3(const int off, const double a, const double b,
                       const double weight)
    {
@@ -36,11 +44,6 @@ OMEGA_H_DEVICE void TetGaussLobatto(const int order = 4) {
       IntPoint(off + 1).Set(a, b, a, weight);
       IntPoint(off + 2).Set(b, a, a, weight);
    }
-  // given a, add the permutations of (a,a,a,b), where 3*a + b = 1
-   void AddTetPoints4(const int off, const double a, const double weight)
-   {
-      IntPoint(off).Set(a, a, a, weight);
-      AddTetPoints3(off + 1, a, 1. - 3.*a, weight); }
   // add the permutations of (a,a,b,b), 2*(a + b) = 1
   void AddTetPoints6(const int off, const double a, const double weight)
   {
@@ -51,13 +54,37 @@ OMEGA_H_DEVICE void TetGaussLobatto(const int order = 4) {
   void AddTetMidPoint(const int off, const double weight)
    { IntPoint(off).Set(0.25, 0.25, 0.25, weight); }
 
-  ir->AddTetPoints4(0, 1./14., 343./45000.);
-  ir[0] = 1./14.;
-  ir[1] = 1./14.;
-  ir[2] = 1./14.;
-  ir[3] = 343./45000.;
-  ir->AddTetMidPoint(4, -74./5625.);
-  ir->AddTetPoints6(5, 0.10059642383320079500, 28./1125.);
+  //AddTetPoints4(0, 1./14., 343./45000.);
+  { 
+    Real a = 1./14.;
+    Real b = 1.-3.*a;
+    Real weight = 343./45000.;
+    ir[0*4+0]=a; ir[0*4+1]=a; ir[0*4+2]=a; ir[0*4+3]=weight;
+    ir[1*4+0]=a; ir[1*4+1]=a; ir[1*4+2]=b; ir[1*4+3]=weight;
+    ir[2*4+0]=a; ir[2*4+2]=b; ir[2*4+1]=a; ir[2*4+3]=weight;
+    ir[3*4+0]=b; ir[3*4+1]=a; ir[3*4+2]=a; ir[3*4+3]=weight;
+  }
+
+  //AddTetMidPoint;
+  { 
+    Real a = 0.25;
+    Real weight = -74./5625.;
+    ir[4*4+0]=a; ir[4*4+1]=a; ir[4*4+2]=a; ir[4*4+3]=weight;
+  }
+
+  //AddTetPoints6(5, 0.10059642383320079500, 28./1125.);
+  {
+    Real a = 0.10059642383320079500;
+    Real b = 0.5-a;
+    Real weight = 28./1125.;
+    ir[5*4+0]=a; ir[5*4+1]=a; ir[5*4+2]=b; ir[5*4+3]=weight;
+    ir[6*4+0]=a; ir[6*4+2]=b; ir[6*4+1]=a; ir[6*4+3]=weight;
+    ir[7*4+0]=b; ir[7*4+1]=a; ir[7*4+2]=a; ir[7*4+3]=weight;
+    swap2(a,b);
+    ir[8*4+0]=a; ir[8*4+1]=a; ir[8*4+2]=b; ir[8*4+3]=weight;
+    ir[9*4+0]=a; ir[9*4+2]=b; ir[9*4+1]=a; ir[9*4+3]=weight;
+    ir[10*4+0]=b; ir[10*4+1]=a; ir[10*4+2]=a; ir[10*4+3]=weight;
+  }
  
   return ir;
 } //end namespace
