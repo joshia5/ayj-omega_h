95e576ff9fe562e98e5145c39d4c0f1d9be39a02
joshia5 <joshia5@rpi.edu>
2023-05-09 19:21:47 -0400

    wrapper for new ctor

--- src/PyOmega_h_file.cpp
+++ src/PyOmega_h_file.cpp
@@ -5,8 +5,12 @@
 namespace Omega_h {
 
 void pybind11_file(py::module& module) {
-  py::class_<Omega_h::filesystem::path>(module, "path")
+  py::class_<filesystem::path>(module, "path")
       .def(py::init<char const*>());
+  //py::class_<Mesh>(module, "Mesh")
+    //  .def(py::init<Mesh>());
+  //filesystem::path (*path_c)(char const* source) = &filesystem::path::path;
+  //filesystem::path (*path_s)(std::string const& source) = &filesystem::path::path; // typename not allowed
   Mesh (*gmsh_read_file)(filesystem::path const&, CommPtr) = &gmsh::read;
   void (*gmsh_write_file)(filesystem::path const&, Mesh*) = &gmsh::write;
   void (*vtk_write_vtu_dim)(std::string const&, Mesh*, Int, bool) =
@@ -16,6 +20,10 @@ void pybind11_file(py::module& module) {
       &vtk::write_parallel;
   void (*vtk_write_parallel)(std::string const&, Mesh*, bool) =
       &vtk::write_parallel;
+  //Mesh (*binary_read_file)
+    //(filesystem::path const&, CommPtr, bool) = &binary::read;
+  Mesh (*binary_read_file)
+    (std::string const&, CommPtr, bool) = &binary::read_s;
   module.def("gmsh_read_file", gmsh_read_file, "Read a Gmsh file");
   module.def("gmsh_write_file", gmsh_write_file, "Write a Gmsh file");
   module.def("vtk_write_vtu", vtk_write_vtu, "Write a mesh as a .vtu file",
@@ -30,6 +38,10 @@ void pybind11_file(py::module& module) {
       "Write entities of one dimension as a directory of parallel VTK files",
       py::arg("path"), py::arg("mesh"), py::arg("cell_dim"),
       py::arg("compress") = true);
+  module.def("binary_read_file", binary_read_file, "Read a .osh file",
+      py::arg("path"), py::arg("comm"), py::arg("strict") = false);
+  //module.def("path_c", path_c, "Byte to path", py::arg("source"));
+  //module.def("path_s", path_s, "String to path", py::arg("source"));
 }
 
 }  // namespace Omega_h
