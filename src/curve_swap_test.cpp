08e3bc1e8d3a81f00bf1cf0c2fe2f99f74d671a5
joshia5 <joshia5@rpi.edu>
2024-07-30 21:07:33 -0400

    working on quartic qual check driver

--- src/curve_swap_test.cpp
+++ src/curve_swap_test.cpp
@@ -352,10 +352,75 @@ void test_annulus_swap(Library *lib) {
   return;
 }
 
+void test_annulus_swap_p4(Library *lib) {
+  auto comm = lib->world();
+
+  auto mesh = meshsim::read("/lore/joshia5/Meshes/curved/annulus-8.sms",
+                            "/lore/joshia5/Models/curved/annulus-8.smd", comm);
+ 
+  calc_quad_ctrlPts_from_interpPts(&mesh);
+  elevate_curve_order_2to3(&mesh);
+  elevate_curve_order_3to4(&mesh);
+  fprintf(stderr, "elevated to order 4\n");
+  mesh.add_tag<Real>(0, "bezier_pts", mesh.dim(), mesh.coords());
+
+  auto wireframe_mesh = Mesh(lib);
+  wireframe_mesh.set_comm(comm);
+  build_quartic_wireframe(&mesh, &wireframe_mesh, 10);
+  std::string vtuPath =
+    "/lore/joshia5/Meshes/curved/annulus-8p4_wire.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &wireframe_mesh, 1);
+  auto quartic_curveVtk_mesh = Mesh(lib);
+  quartic_curveVtk_mesh.set_comm(comm);
+  build_quartic_curveVtk(&mesh, &quartic_curveVtk_mesh, 10);
+  vtuPath = "/lore/joshia5/Meshes/curved/annulusp4-8.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &quartic_curveVtk_mesh, 2);
+
+  auto opts = AdaptOpts(&mesh);
+  opts.should_coarsen = false;
+  opts.should_coarsen_slivers = false;
+  opts.should_refine = false;
+  opts.should_filter_invalids = false;
+  opts.verbosity = EXTRA_STATS;
+  opts.min_quality_desired = 0.99;
+  opts.min_quality_allowed = 0.98;
+  mesh.add_tag<Real>(VERT, "metric", 1);
+  mesh.set_tag(VERT, "metric", Reals(mesh.nverts(), 1));
+  /*
+  auto valid_tris_bef = checkValidity_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
+  for (LO adapt_itr = 0; adapt_itr < 1; ++adapt_itr) {
+    fprintf(stderr, "itr %d\n", adapt_itr);
+    swap_edges(&mesh, opts);
+  }
+
+  wireframe_mesh = Mesh(lib);
+  wireframe_mesh.set_comm(comm);
+  build_cubic_wireframe_2d(&mesh, &wireframe_mesh, 4);
+  vtuPath =
+    "/lore/joshia5/Meshes/curved/annulus-8-swap_wire.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &wireframe_mesh, 1);
+  cubic_curveVtk_mesh = Mesh(lib);
+  cubic_curveVtk_mesh.set_comm(comm);
+  build_cubic_curveVtk_2d(&mesh, &cubic_curveVtk_mesh, 4);
+  vtuPath = "/lore/joshia5/Meshes/curved/annulus-8-swap.vtu";
+  vtk::write_simplex_connectivity(vtuPath.c_str(), &cubic_curveVtk_mesh, 2);
+  auto quals = askQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
+
+  fprintf(stderr, "finish swapping annulus-2d\n");
+  vtk::FullWriter writer;
+  writer = vtk::FullWriter(
+      "/lore/joshia5/Meshes/curved/annulus-8-swap_full.vtk", &mesh);
+  writer.write();
+  //mesh.ask_qualities();
+  */
+
+  return;
+}
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
 
   test_annulus_swap(&lib); //2d
+  test_annulus_swap_p4(&lib); //2d
   //test_disc_swap(&lib); //2d
   //test_swap_kova(&lib); //3d
   
