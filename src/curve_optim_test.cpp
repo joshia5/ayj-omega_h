1733f95e4c5c4d55a36a201932ea04bf43b8daa0
joshia5 <joshia5@rpi.edu>
2024-12-16 13:08:34 -0500

    working on driver; input mesh construction

--- src/curve_optim_test.cpp
+++ src/curve_optim_test.cpp
@@ -73,15 +73,15 @@ void test_annuluswithEigen(Library *lib) {
   mesh_4tri.add_tag<Real>(0, "bezier_pts", mesh_4tri.dim(), mesh_4tri.coords());
   auto const dim = mesh_4tri.dim();
 
-  auto cubic_2tri = Mesh(lib);
-  cubic_2tri.set_comm(comm);
-  cubic_2tri.set_parting(OMEGA_H_ELEM_BASED);
-  cubic_2tri.set_dim(dim);
-  cubic_2tri.set_family(OMEGA_H_SIMPLEX);
+  auto mesh = Mesh(lib);
+  mesh.set_comm(comm);
+  mesh.set_parting(OMEGA_H_ELEM_BASED);
+  mesh.set_dim(dim);
+  mesh.set_family(OMEGA_H_SIMPLEX);
   LO const numVtx=4;
   LO const numEdge=5;
   LO const numFace=2;
-  cubic_2tri.set_verts(numVtx);
+  mesh.set_verts(numVtx);
 
   HostWrite<Real> host_coords(numVtx*dim);
   host_coords = {
@@ -94,54 +94,54 @@ void test_annuluswithEigen(Library *lib) {
   class_id_0 = {12,11,16,14};
   HostWrite<I8> class_dim_0(numVtx);// all 0s
   class_dim_0 = {0,0,0,0};
-  cubic_2tri.add_coords(Reals(host_coords.write()));
-  cubic_2tri.add_tag<ClassId>(0, "class_id", 1,
+  mesh.add_coords(Reals(host_coords.write()));
+  mesh.add_tag<ClassId>(0, "class_id", 1,
                            Read<LO>(class_id_0.write()));
-  cubic_2tri.add_tag<I8>(0, "class_dim", 1,
+  mesh.add_tag<I8>(0, "class_dim", 1,
                       Read<I8>(class_dim_0.write()));
 
   HostWrite<LO> ev2v(numEdge*2);
   ev2v = {0,1,  1,2,  2,3,  3,0, 1,3};
-  cubic_2tri.set_ents(1, Adj(Read<LO>(ev2v.write())));
+  mesh.set_ents(1, Adj(Read<LO>(ev2v.write())));
   HostWrite<LO> class_id_1(numEdge);
   class_id_1 = {13,7,17,15,2};
   HostWrite<I8> class_dim_1(numEdge);// all 0s
   class_dim_1 = {1,1,1,1,2};
-  cubic_2tri.add_tag<ClassId>(1, "class_id", 1,
+  mesh.add_tag<ClassId>(1, "class_id", 1,
                            Read<LO>(class_id_1.write()));
-  cubic_2tri.add_tag<I8>(1, "class_dim", 1,
+  mesh.add_tag<I8>(1, "class_dim", 1,
                       Read<I8>(class_dim_1.write()));
  
   HostWrite<LO> fv2v(numFace*3);
   fv2v = {0,1,3,  2,3,1};
-  //cubic_2tri.set_down(2, 0, LOs(fv2v.write()));//cant set if using set_ents
-  Adj edge2vert = cubic_2tri.get_adj(1, 0);
-  Adj vert2edge = cubic_2tri.ask_up(0, 1);
+  //mesh.set_down(2, 0, LOs(fv2v.write()));//cant set if using set_ents
+  Adj edge2vert = mesh.get_adj(1, 0);
+  Adj vert2edge = mesh.ask_up(0, 1);
   Adj f2e = reflect_down(LOs(fv2v.write()), edge2vert.ab2b, vert2edge,
       OMEGA_H_SIMPLEX, 2, 1);
-  cubic_2tri.set_ents(2, f2e);
+  mesh.set_ents(2, f2e);
   printf("ok5\n");
   HostWrite<LO> class_id_2(numFace);
   class_id_2 = {2,2};
   HostWrite<I8> class_dim_2(numFace);// all 0s
   class_dim_2 = {2,2};
-  cubic_2tri.add_tag<ClassId>(2, "class_id", 1,
+  mesh.add_tag<ClassId>(2, "class_id", 1,
                            Read<LO>(class_id_2.write()));
-  cubic_2tri.add_tag<I8>(2, "class_dim", 1,
+  mesh.add_tag<I8>(2, "class_dim", 1,
                       Read<I8>(class_dim_2.write()));
-  for (LO i = 0; i <= cubic_2tri.dim(); ++i) {
-    if (!cubic_2tri.has_tag(i, "global")) {
-      cubic_2tri.add_tag(i, "global", 1, Omega_h::GOs(cubic_2tri.nents(i), 0, 1));
+  for (LO i = 0; i <= mesh.dim(); ++i) {
+    if (!mesh.has_tag(i, "global")) {
+      mesh.add_tag(i, "global", 1, Omega_h::GOs(mesh.nents(i), 0, 1));
     }
   }
   vtk::FullWriter writer;
   writer = vtk::FullWriter(
-      "/lore/joshia5/Meshes/curved/annulus-120-2tri_full.vtk", &cubic_2tri);
+      "/lore/joshia5/Meshes/curved/annulus-120-2tri_full.vtk", &mesh);
   writer.write();
-  cubic_2tri.set_curved(1);
-  cubic_2tri.set_max_order(3);
-  cubic_2tri.add_tags_for_ctrlPts();
-  cubic_2tri.add_tag<Real>(0, "bezier_pts", cubic_2tri.dim(), cubic_2tri.coords());
+  mesh.set_curved(1);
+  mesh.set_max_order(3);
+  mesh.add_tags_for_ctrlPts();
+  mesh.add_tag<Real>(0, "bezier_pts", mesh.dim(), mesh.coords());
   printf("making 2tri curve mesh\n");
 
   HostWrite<Real> edgePt_coords(numEdge*dim*2);
@@ -203,22 +203,9 @@ void test_annuluswithEigen(Library *lib) {
         host_coords[v1*dim + d] + host_coords[v2*dim + d])/3.;
     }
   }
-  cubic_2tri.set_tag_for_ctrlPts(1, Reals(edgePt_coords.write()));
-  cubic_2tri.set_tag_for_ctrlPts(2, Reals(facePt_coords.write()));
+  mesh.set_tag_for_ctrlPts(1, Reals(edgePt_coords.write()));
+  mesh.set_tag_for_ctrlPts(2, Reals(facePt_coords.write()));
 
-  auto opts = AdaptOpts(&cubic_2tri);
-  opts.should_coarsen = false;
-  opts.should_coarsen_slivers = false;
-  opts.should_refine = false;
-  opts.should_filter_invalids = false;
-  opts.verbosity = EXTRA_STATS;
-  opts.min_quality_desired = 0.99;
-  opts.min_quality_allowed = 0.98;
-  cubic_2tri.add_tag<Real>(VERT, "metric", 1);
-  cubic_2tri.set_tag(VERT, "metric", Reals(cubic_2tri.nverts(), 1));
-  auto valid_tris_bef = checkValidity_2d(&cubic_2tri, LOs(cubic_2tri.nfaces(), 0, 1), 2);
-
-  /*
   auto opts = AdaptOpts(&mesh);
   opts.should_coarsen = false;
   opts.should_coarsen_slivers = false;
@@ -230,14 +217,13 @@ void test_annuluswithEigen(Library *lib) {
   mesh.add_tag<Real>(VERT, "metric", 1);
   mesh.set_tag(VERT, "metric", Reals(mesh.nverts(), 1));
   auto valid_tris_bef = checkValidity_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
-  */
-  askWorstQuality_2d(&cubic_2tri, LOs(cubic_2tri.nfaces(), 0, 1), 2);
-  //auto quals = askQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
+
+  askWorstQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
   for (LO adapt_itr = 0; adapt_itr < 1; ++adapt_itr) {
     fprintf(stderr, "itr %d\n", adapt_itr);
-    swap_edges(&cubic_2tri, opts);
+    swap_edges(&mesh, opts);
   }
-
+  askWorstQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
 
 /*
   const int n = 8;//number of internal ctrl pts*dim // for 2d 2tri, 1edge case =8
@@ -659,5 +645,8 @@ int main(int argc, char** argv) {
   //
   
   test_rosenbrock();
+
+  test_annuluswithEigen(&lib);
+
   return 0;
 }
